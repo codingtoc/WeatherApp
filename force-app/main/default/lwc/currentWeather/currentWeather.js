8afacad4cb7e05e563383c203091d01ec7f93a2a
codingtoc <codingtoc@gmail.com>
2024-11-28 13:19:48 +0900

    currentWeather Component 디자인 및 조회 기능 구현

--- force-app/main/default/lwc/currentWeather/currentWeather.js
+++ force-app/main/default/lwc/currentWeather/currentWeather.js
@@ -1,3 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import getCurrentWeather from "@salesforce/apex/WeatherAPI.getCurrentWeather";
 
-export default class CurrentWeather extends LightningElement {}
+export default class CurrentWeather extends LightningElement {
+  location = "";
+  locationName;
+  locationCountry;
+  weatherIcon;
+  weatherText;
+  currentTemperature;
+  showInfo = false;
+
+  connectedCallback() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.location =
+          position.coords.latitude + "," + position.coords.longitude;
+        this.handleButtonClick();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  handleInputChange(event) {
+    this.location = event.target.value;
+  }
+
+  handleButtonClick() {
+    getCurrentWeather({ location: this.location })
+      .then((result) => {
+        this.showInfo = true;
+        let weatherData = JSON.parse(result);
+        this.locationName = weatherData.location.name;
+        this.locationCountry = weatherData.location.country;
+        this.weatherIcon = weatherData.current.condition.icon;
+        this.weatherText = weatherData.current.condition.text;
+        this.currentTemperature = weatherData.current.temp_c;
+      })
+      .catch((error) => {
+        this.showInfo = false;
+        console.log(error);
+      });
+  }
+}
